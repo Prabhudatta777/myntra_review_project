6c226659f122409a88f9978725003e537f0130a3
Prabhudatta777 <prabhudattas57@gmail.com>
2026-07-14 11:08:27 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages , setup
+
+
+setup(
+    name = 'scrapper' ,
+    version = '0.0.1' ,
+    author = 'Prabhudatta' ,
+    author_email = 'prabhudattas57@gmail.com' ,
+    packages = find_packages() ,
+    install_requires = []
+)
